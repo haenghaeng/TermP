7688ef5db296261423fedb863bbcff516be9f613
haenghaeng <haeng@pusan.ac.kr>
2024-05-28 13:06:56 +0900

    feat : MainActivity, build.gradle.kts
    1. CameraX 라이브러리에 대한 의존성 추가
    2. PreviewView에 현재 카메라 화면 띄우기

--- app/src/main/java/com/example/termp/MainActivity.kt
+++ app/src/main/java/com/example/termp/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.termp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceView
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 
 class MainActivity : AppCompatActivity() {
     lateinit var gallery_btn : ImageButton
     lateinit var camera_btn : ImageButton
     lateinit var function_btn : ImageButton
 
-//    lateinit var cameraView: CameraView
+    lateinit var previewView: PreviewView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +44,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         // view 연결
-//        cameraView = findViewById(R.id.cameraView)
+        previewView = findViewById(R.id.previewView)
 
-        // 카메라 preview를 surfaceView에 띄우기
-        // ...
+        // 카메라 실행
+        startCamera()
     }
 
     fun gallery_btn_click(){
@@ -54,6 +61,39 @@ class MainActivity : AppCompatActivity() {
     fun function_btn_click(){
         // ?
         Toast.makeText(this, "function", Toast.LENGTH_SHORT).show();
+    }
+
+
+    /**
+     * 카메라 실행 함수
+     * https://developer.android.com/media/camera/camerax/preview?hl=ko
+     * 현재 화면이 약간 잘려서 나옴. 수정 해야 함
+     */
+    fun startCamera(){
+        // 1. CameraProvider 요청
+        // ProcessCameraProvider는 Camera의 생명주기를 LifeCycleOwner의 생명주기에 Binding 함
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        // 2. CameraProvier 사용 가능 여부 확인
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+
+
+    fun bindPreview(cameraProvider : ProcessCameraProvider) {
+        var preview : Preview = Preview.Builder()
+            .build()
+
+        var cameraSelector : CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider())
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
     }
 
 
